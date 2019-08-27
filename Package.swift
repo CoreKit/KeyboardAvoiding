454e76a8c093f5f9b65aae0aa4128599334dfcdb
Tibor Bödecs <mail.tib@gmail.com>
2019-08-27 16:20:24 +0200

    removed test target

--- Package.swift
+++ Package.swift
@@ -19,8 +19,5 @@ let package = Package(
             name: "KeyboardAvoiding",
             dependencies: [],
             path: "./Sources"),
-        .testTarget(
-            name: "KeyboardAvoidingTests",
-            dependencies: ["KeyboardAvoiding"]),
     ]
 )
